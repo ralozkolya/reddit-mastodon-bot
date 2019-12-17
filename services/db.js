1cb21e20b525ae775073da20cf3ced329b3d0352
Nikoloz Razmadze <raloz1@gmail.com>
2019-12-18 00:59:25 +0400

    Define charset (mostly to allow emojis in title)

--- services/db.js
+++ services/db.js
@@ -3,8 +3,8 @@ const { host, database, username: user, password } = process.env;
 const knex = require('knex')({
     client: 'mysql',
     connection: {
-        host, database, user,  password
-    }
+        host, database, user,  password, charset: 'utf8mb4'
+    },
 });
 
 async function getFiltered(list = []) {
